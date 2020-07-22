f4c7a705b2cddf06d5e2bbe722e5ee4e40a010f6
sanke <egor.tyukin@gmail.com>
2020-07-22 18:32:31 +0300

    add pathfinder.cs

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+  Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    public void LoadBlocks()
+    {
+      var waypoints = FindObjectsOfType<Waypoint>();
+      foreach (Waypoint waypoint  in waypoints)
+      {
+        var gridPos = waypoint.GetGridPos();
+        if (grid.ContainsKey(gridPos)) {
+          Debug.LogWarning("Overlapping block " + waypoint);
+        } else {
+          grid.Add(gridPos, waypoint);
+        }
+        print("Loaded " + grid.Count + " blocks");
+        
+      }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
