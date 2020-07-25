b61a2679292aa8a5ce1d8fff4ee6cba9666c178d
sanke <egor.tyukin@gmail.com>
2020-07-25 18:09:09 +0300

    breadth first search added minor refactor

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -13,24 +13,62 @@ public class PathFinder : MonoBehaviour
     Vector2Int.left
   };
   Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+  Queue<Waypoint> queue = new Queue<Waypoint>();
+  bool isRunning = true;
+  Waypoint searchCenter;
     // Start is called before the first frame update
     void Start()
     {
       ColorStartAndEnd();
       LoadBlocks();
+      PathFind();
+      //ExploreNeighbours();
+    }
+
+  private void PathFind()
+  {
+    queue.Enqueue(startWaypoint);
+    while (queue.Count > 0 && isRunning) {
+      searchCenter = queue.Dequeue();
+      searchCenter.isExplored = true;
+      HaltIfEndFound();
       ExploreNeighbours();
     }
+    print("Finished");
+  }
+
+  private void HaltIfEndFound()
+  {
+    if (searchCenter == endWaypoint) {
+      print("Found the end");
+      isRunning = false;
+    }
+  }
 
   private void ExploreNeighbours()
   {
+    if (!isRunning) { return; }
     foreach (Vector2Int direction in directions)
     {
-      Vector2Int explorationCoordinates = direction + startWaypoint.GetGridPos();
-      try {
-        grid[explorationCoordinates].SetTopColor(Color.blue);
-      } catch {
-        print("no grid element");
+      Vector2Int neighbourCoordinates = searchCenter.GetGridPos() + direction;
+      try
+      {
+        QueueNewNeighbours(neighbourCoordinates);
       }
+      catch {
+        
+      }
+    }
+  }
+
+  private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+  {
+    Waypoint neighbour = grid[neighbourCoordinates];
+    if (neighbour.isExplored || queue.Contains(neighbour)) {
+      // do nothing
+    } else {
+      queue.Enqueue(neighbour);
+      neighbour.exploredFrom = searchCenter;
     }
   }
 
@@ -51,7 +89,7 @@ public class PathFinder : MonoBehaviour
         } else {
           grid.Add(gridPos, waypoint);
         }
-        print("Loaded " + grid.Count + " blocks");
+        //print("Loaded " + grid.Count + " blocks");
         
       }
       
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+  [SerializeField] Color exploredColor;
+  public bool isExplored = false;
+  public Waypoint exploredFrom;
   const int gridSize = 10;
   Vector2Int gridPos;
     public int GetGridSize()
@@ -22,5 +25,11 @@ public class Waypoint : MonoBehaviour
     {
       MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
       topMeshRenderer.material.color = color;
+    }
+
+    public void Update() {
+      if (isExplored) {
+        //SetTopColor(exploredColor);
+      }
     }
 }
