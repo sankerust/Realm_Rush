a80b72fd5a9f8991272269632d933f5f64ec3442
sanke <egor.tyukin@gmail.com>
2020-07-28 10:58:39 +0300

    solo challenge Add damage system to enemy add simple vfx WIP

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+  [SerializeField] GameObject hitFx;
+  [SerializeField] int hitsToDie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private void OnParticleCollision(GameObject other) {
+      print("particle collision");
+      GameObject enemyHitFx = Instantiate(hitFx, transform.position, Quaternion.identity);
+      hitsToDie--;
+      if (hitsToDie <= 1) {
+        Destroy(gameObject);
+      }
+      //enemyHitFx.transform.parent = parent;
+    }
+}
