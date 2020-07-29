1a773ec789e36d5bbc7b46bd38173f1aa41d77cf
sanke <egor.tyukin@gmail.com>
2020-07-29 21:48:25 +0300

    minor refactor according to lecture material

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
   [SerializeField] GameObject hitFx;
-  [SerializeField] int hitsToDie;
+  [SerializeField] int hitPoints = 15;
 
     // Start is called before the first frame update
     void Start()
     {
     }
-
    private void OnParticleCollision(GameObject other) {
       print("particle collision");
       GameObject enemyHitFx = Instantiate(hitFx, transform.position, Quaternion.identity);
-      hitsToDie--;
-      if (hitsToDie <= 1) {
-        Destroy(gameObject);
+      ProcessHit();
+      if (hitPoints <= 1)
+      {
+        KillEnemy();
       }
       //enemyHitFx.transform.parent = parent;
+    }
+    private void KillEnemy()
+    {
+      Destroy(gameObject);
+    }
+    private void ProcessHit()
+    {
+      hitPoints--;
     }
 }
