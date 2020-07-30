7b4718f513cdda78c55aeb0499e2076948f70ac3
sanke <egor.tyukin@gmail.com>
2020-07-30 19:26:55 +0300

    Coroutine enemy spawning enabled Multiple enemies support

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+  [SerializeField] float secondsBetweenSpawn = 2f;
+  [SerializeField] EnemyMovement enemyPrefab;
+  [SerializeField] int unitCount;
+    void Start()
+    {
+      StartCoroutine(SpawnEnemyUnit());
+    }
+
+  private IEnumerator SpawnEnemyUnit()
+  {
+    while (unitCount > 0) {
+      Instantiate(enemyPrefab);
+      unitCount--;
+      yield return new WaitForSeconds(secondsBetweenSpawn);
+    }
+  }
+}
