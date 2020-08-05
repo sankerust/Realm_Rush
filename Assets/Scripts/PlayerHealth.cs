011061d2596a05f7709e428e048f5a9061bfe63f
sanke <egor.tyukin@gmail.com>
2020-08-05 21:13:47 +0300

    add player health && decrease on base impact

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+  [SerializeField] int playerHP = 5;
+    private void OnTriggerEnter(Collider other) {
+      playerHP--;
+    }
+}
